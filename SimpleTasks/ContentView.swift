d974a800de39168e8b127cd5c3977a6be353d03e
Eugene Honcharenko <eugene@tulusha.com>
2021-01-02 11:13:29 +0200

    Initial Commit

---
+++ SimpleTasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleTasks
+//
+//  Created by Eugene Honcharenko on 1/2/21.
+//  Copyright © 2021 Honcharenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
